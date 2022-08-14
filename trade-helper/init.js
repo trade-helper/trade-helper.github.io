2483e1afe1fd4239432ebc69ac2ec4bce03369a4
Marhanfar <null>
2022-08-14 15:37:27 +0200

    update script and index.html

--- trade-helper/init.js
+++ trade-helper/init.js
@@ -1,11 +1,17 @@
 const { execSync } = require("child_process");
 
 const main = () => {
+    // backup modified index.html
+    execSync("cp index.html index-backup.html");
+    // install, build, copy dist to root
     execSync("rm -rf ./awakened-poe-trade");
     execSync('git clone https://github.com/SnosMe/awakened-poe-trade.git');
     execSync('cd awakened-poe-trade/renderer && npm install && npm run make-index-files && npm run build');
     execSync('cp -r awakened-poe-trade/renderer/dist/. .');
-    execSync("rm -rf ./awakened-poe-trade");
+    // revert index.html
+    execSync("cp index-backup.html index.html");
+    // clean up
+    execSync("rm -rf ./awakened-poe-trade index-backup.html");
 };
 
 main();
