23fda371f3115d552b5209bca3cc60b27873175e
Marhanfar <null>
2022-08-14 19:32:43 +0200

    update implementation

--- init.js
+++ init.js
@@ -1,19 +1,32 @@
 const { execSync } = require("child_process");
 
 const main = () => {
-    // backup modified index.html
+    // backup modified index.html and clear possible leftover files
     execSync("cp index.html index-backup.html");
-    // install, build, copy dist to root
     execSync("rm -rf ./awakened-poe-trade");
+
+    // clone
     execSync('git clone https://github.com/SnosMe/awakened-poe-trade.git');
+
+    // rename assets to avoid problems with github pages
     execSync("sed -i 's/index.bin/bin/g' awakened-poe-trade/renderer/src/assets/make-index-files.mjs")
     execSync("sed -i 's/index.bin/bin/g' awakened-poe-trade/renderer/src/assets/data/index.ts")
+
+    // build app and move it to page root
     execSync('cd awakened-poe-trade/renderer && npm install && npm run make-index-files && npm run build');
     execSync('cp -r awakened-poe-trade/renderer/dist/. .');
-    // revert index.html
-    execSync("cp index-backup.html index.html");
+
+    // revert index.html to original
+    execSync("mv index-backup.html index.html");
+
+    // rename files to avoid problems with github pages
+    execSync("cd awakened-poe-trade/renderer/src/assets && sed -i 's/index.bin/bin/g' ./make-index-files.mjs && sed -i 's/index.bin/bin/g' ./data/index.ts")
+
+    // remove hashes from css and js file names so our index.html won't change
+    execSync('cd assets && ren *.js index.js && ren *.css index.css');
+
     // clean up
-    execSync("rm -rf ./awakened-poe-trade index-backup.html");
+    execSync("rm -rf ./awakened-poe-trade");
 };
 
 main();
